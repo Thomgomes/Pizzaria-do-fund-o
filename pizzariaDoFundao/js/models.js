a6b0e9902f452ba3ab60e1f7d2866fe2b7da68d8
ThomGomes <89878334+Thomgomes@users.noreply.github.com>
2022-06-17 19:55:39 -0300

    Add files via upload

---
+++ pizzariaDoFundao/js/models.js
@@ -0,0 +1,8 @@
+let modelsJson = [
+    {id:1, name:'Pizza de Frango com Catupiry', img:'./imgs/frangocatu.jpg', price:[29.99, 24.99, 19.99], sizes:['38 cm', '31 cm', '25 cm'], description:'Uma deliciosa pizza de Frango com catupiry!'},
+    {id:2, name:'Pizza 4 Queijos', img:'./imgs/4queijos.jpg', price:[29.99, 24.99, 19.99], sizes:['38 cm', '31 cm', '25 cm'], description:'Uma deliciosa pizza de 4 queijos!'},
+    {id:3, name:'Pizza de Alho', img:'./imgs/alho.jpg', price:[29.99, 24.99, 19.99], sizes:['38 cm', '31 cm', '25 cm'], description:'Uma deliciosa pizza de alho (goste quem gostar....)!'},
+    {id:4, name:'Pizza de Bacon', img:'./imgs/bacon.jpg', price:[29.99, 24.99, 19.99], sizes:['38 cm', '31 cm', '25 cm'], description:'Uma deliciosa pizza de bacon!'},
+    {id:5, name:'Pizza de Calabresa', img:'./imgs/calabresa.jpg', price:[29.99, 24.99, 19.99], sizes:['38 cm', '31 cm', '25 cm'], description:'Uma deliciosa pizza de calabresa!'},
+    {id:6, name:'Pizza de Milho', img:'./imgs/milho.jpg', price:[29.99, 24.99, 19.99], sizes:['38 cm', '31 cm', '25 cm'], description:'Uma deliciosa pizza de milho!'},
+];
